65d29e3aa76b5d81a3e5b0e1de828a80ef7f4759
Soaprman <m.grieshober@gmail.com>
2020-10-17 16:51:40 -0400

    Fix jank with applyUserscriptHeader - thanks gulp-transform!

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,16 +1,23 @@
+// Gulp
 const gulp = require('gulp');
+const stream = require('vinyl-source-stream');
+const transform = require('gulp-transform');
+
+// For styles
 const sass = require('gulp-sass');
+
+// For scripts
 const browserify = require('browserify');
-const vinylBuffer = require('vinyl-buffer');
-const vinylSourceStream = require('vinyl-source-stream');
-const applyUserscriptHeader = require('./streams/applyUserscriptHeader');
+
+// Custom stuff
+const applyUserscriptHeader = require('./transforms/applyUserscriptHeader');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('src/scss/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('output'));
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('output'));
 });
 
 gulp.task('sass:watch', function () {
@@ -22,10 +29,10 @@ gulp.task('styles', gulp.series('sass'));
 
 gulp.task('scripts', function () {
     return browserify('./src/js/app.js')
-    .bundle()
-    .pipe(applyUserscriptHeader())
-    .pipe(vinylSourceStream('bundle.js'))
-    .pipe(gulp.dest('output'));
+        .bundle()
+        .pipe(stream('bundle.js'))
+        .pipe(transform('utf8', applyUserscriptHeader))
+        .pipe(gulp.dest('output'));
 });
 
 
---
+++ gulpfile.js/transforms/applyUserscriptHeader.js
@@ -0,0 +1,18 @@
+module.exports = function (content, file) {
+    let userscriptHeader = 
+`// ==UserScript==
+// @name         AWBW Cool Script
+// @namespace    http://tampermonkey.net/
+// @version      2.0.0
+// @description  Adds a bunch of features to Advance Wars By Web. Wow!
+// @author       Soaprman
+// @match        https://awbw.amarriner.com/*
+// @icon         https://thedailypos.org/misc/awtank.png
+// @require		 https://code.jquery.com/jquery-3.5.1.min.js
+// @grant        none
+// ==/UserScript==
+    
+    `;
+
+    return userscriptHeader + content;
+};
